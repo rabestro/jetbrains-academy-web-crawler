743ae1fa2111842fa4017051d9478e5298cce36c
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 23:07:29 +0300

    Stage 1 solved

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
+        setTitle("Web Crawler");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setVisible(true);
-        setTitle("Web Crawler");
 
         final var textArea = new JTextArea("HTML code?");
         textArea.setName("TextArea");
         textArea.setEnabled(false);
+
         add(textArea);
     }
 }
