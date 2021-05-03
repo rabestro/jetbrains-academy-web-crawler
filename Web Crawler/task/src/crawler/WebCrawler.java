a6274cc86f892a426838aa43994a4810dc8ea2ef
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 23:27:33 +0300

    Add components

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -2,17 +2,32 @@ package crawler;
 
 import javax.swing.*;
 
+import java.awt.*;
+
+import static java.lang.System.Logger.Level.INFO;
+
 public class WebCrawler extends JFrame {
-    public WebCrawler() {
+    private static final System.Logger LOGGER = System.getLogger("");
+
+    private final JButton runButton = new JButton("Get text!");
+    private final JTextField url = new JTextField();
+    private final JTextArea textArea = new JTextArea("HTML code?");
+    {
         setTitle("Web Crawler");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setVisible(true);
 
-        final var textArea = new JTextArea("HTML code?");
-        textArea.setName("TextArea");
+        url.setName("UrlTextField");
+        add(url, BorderLayout.NORTH);
+        runButton.setName("RunButton");
+        add(runButton, BorderLayout.SOUTH);
+        textArea.setName("HtmlTextArea");
         textArea.setEnabled(false);
+        add(textArea, BorderLayout.CENTER);
+    }
 
-        add(textArea);
+    public WebCrawler() {
+        LOGGER.log(INFO, "Web Crawler started!");
     }
 }
