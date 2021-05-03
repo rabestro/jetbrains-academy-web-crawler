a36028274463afa87acdad9edeaf362eaa2a63bb
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 23:33:01 +0300

    Solved stage 2

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -3,14 +3,22 @@ package crawler;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static java.lang.System.Logger.Level.INFO;
 
-public class WebCrawler extends JFrame {
+public class WebCrawler extends JFrame implements ActionListener {
     private static final System.Logger LOGGER = System.getLogger("");
 
     private final JButton runButton = new JButton("Get text!");
-    private final JTextField url = new JTextField();
+    private final JTextField urlField = new JTextField();
     private final JTextArea textArea = new JTextArea("HTML code?");
     {
         setTitle("Web Crawler");
@@ -18,9 +26,10 @@ public class WebCrawler extends JFrame {
         setSize(300, 300);
         setVisible(true);
 
-        url.setName("UrlTextField");
-        add(url, BorderLayout.NORTH);
+        urlField.setName("UrlTextField");
+        add(urlField, BorderLayout.NORTH);
         runButton.setName("RunButton");
+        runButton.addActionListener(this);
         add(runButton, BorderLayout.SOUTH);
         textArea.setName("HtmlTextArea");
         textArea.setEnabled(false);
@@ -29,5 +38,18 @@ public class WebCrawler extends JFrame {
 
     public WebCrawler() {
         LOGGER.log(INFO, "Web Crawler started!");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        LOGGER.log(INFO, "actionPerformed: " + e);
+        try (InputStream inputStream = new BufferedInputStream(new URL(urlField.getText()).openStream())) {
+            String siteText = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            textArea.setText(siteText);
+        } catch (MalformedURLException malformedURLException) {
+            malformedURLException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
