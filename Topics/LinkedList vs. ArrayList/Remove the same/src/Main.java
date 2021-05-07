b5ef2979fd33f4ff7eaefdecbbe1da3c0584a770
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-07 12:27:00 +0300

    Update solution for daily problem

--- Topics/LinkedList vs. ArrayList/Remove the same/src/Main.java
+++ Topics/LinkedList vs. ArrayList/Remove the same/src/Main.java
@@ -3,10 +3,12 @@ import java.util.*;
 class ListOperations {
     public static void removeTheSame(LinkedList<String> linkedList, ArrayList<String> arrayList) {
         var al = arrayList.iterator();
-        for (var ll = linkedList.iterator(); ll.hasNext(); )
+        var ll = linkedList.iterator();
+        while (ll.hasNext()) {
             if (ll.next().equals(al.next())) {
                 ll.remove();
                 al.remove();
             }
+        }
     }
 }
